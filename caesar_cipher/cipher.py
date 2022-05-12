38e3d75056af22b4537891561c3750d4536128b8
Steve Ngo <stevehngo@hotmail.com>
2022-05-12 07:58:20 -0400

    finished crack method

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -1,4 +1,5 @@
 from .corpus_loader import word_list, name_list
+import re
 
 
 def encrypt(eggs, key):
@@ -33,6 +34,36 @@ def decrypt(scrambled, key):
   # decrypts a string
   return encrypt(scrambled, 26-key)
 
-def crack():
-  # cracks an encoded string
-  pass
+def crack(shells):
+    # finds possible solution to scrambled eggs, i mean words...
+    humpty_dumpty = []
+    for i in range(26):
+        humpty_dumpty.append(encrypt(shells, i))
+
+    # code below taken and altered from demo:
+    for i in humpty_dumpty:
+        word_count = count_words(i)
+        percentage = int(word_count / len(i.split()) * 100)
+        if percentage > 50:
+            return i
+    return ""
+
+# taken from demo
+def count_words(text):
+    # returns a count of real words
+    candidate_words = text.split()
+
+    word_count = 0
+
+    for candidate in candidate_words:
+        word = re.sub(r'[^A-Za-z]+','', candidate)
+        if word.lower() in word_list or word in name_list:
+            # print("english word", word)
+            word_count += 1
+        else:
+            pass
+            # print('not english word or name', word)
+
+    return word_count
+
+
